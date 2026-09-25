9a9da5b1a145c668f80c7c1b60736a7ff369f27f
DevNickJr <nicholasduadei14@gmail.com>
2026-09-25 23:56:17 +0100

    fix build

--- src/infrastructure/database/app-data-source.ts
+++ src/infrastructure/database/app-data-source.ts
@@ -13,5 +13,5 @@ export const AppDataSource = new DataSource({
       '/../../modules/**/infrastructure/persistence/*.orm-entity.{js,ts}',
   ],
   logging: env.NODE_ENV === 'development',
-  synchronize: env.NODE_ENV === 'development',
+  synchronize: true,
 });
